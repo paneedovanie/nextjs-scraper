ed9ae03cf4ae170a42eb5fbcd800e75d37ef92b0
edhiramis-firstmate <edhiramis@firstmate.tech>
2024-08-19 16:58:09 +0800

    Added scraper

--- app/api/scraper/route.ts
+++ app/api/scraper/route.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import puppeteerCore from "puppeteer-core";
-import puppeteer from "puppeteer";
+import puppeteer, { Browser } from "puppeteer";
 import chromium from "@sparticuz/chromium";
 
 export const dynamic = "force-dynamic";
 
-async function getBrowser() {
+async function getBrowser(): Promise<Browser> {
   if (process.env.VERCEL_ENV === "production") {
     const executablePath = await chromium.executablePath();
 
@@ -15,7 +15,7 @@ async function getBrowser() {
       executablePath,
       headless: chromium.headless,
     });
-    return browser;
+    return browser as unknown as Browser;
   } else {
     const browser = await puppeteer.launch();
     return browser;
@@ -27,6 +27,16 @@ export async function GET(request: NextRequest) {
 
   const page = await browser.newPage();
   await page.goto("https://example.com");
+
+  const { title, h1 } = await page.evaluate(() => {
+    const title = document.querySelector("title");
+    const h1 = document.querySelector("h1");
+
+    return { title: title?.innerText, h1: h1?.innerHTML };
+  });
+
+  console.log(title);
+
   await browser.close();
-  return new NextResponse("Hello!");
+  return new NextResponse(`Title: ${title}, Header: ${h1}`);
 }
